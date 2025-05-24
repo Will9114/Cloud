7f13bc71eab7d73a2f81e32dcca314f9bd3c3a7d
Will9114 <91405662+Will9114@users.noreply.github.com>
2025-05-23 22:46:50 -0600

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -154,7 +154,7 @@
 
     <?php
     // Configuración de conexión a la base de datos
-    $host = "10.10.0.4"; // Por ejemplo: "localhost"
+    $host = "20.51.9.131"; // Por ejemplo: "localhost"
     $dbname = "mascotas_db";
     $username = "william";
     $password = "alfonso27";
